57836c5a2e33576d57c527f26a9144ac0133d123
peterpeterparker <david.dalbusco@outlook.com>
2021-02-11 08:31:12 +0100

    feat: add tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.scss';
 
 import Link from 'next/link'
 
@@ -14,13 +14,17 @@ export const Home = ({ allPosts }) => {
       </Head>
 
       <main>
-        {
-          allPosts.map(post => {
-            return <Link as={`/playlist/${post.slug}`} href="/playlist/[slug]" key={post.slug}>
-              {post.frontmatter.name}
-            </Link>
-          })
-        }
+          <div className="container mx-auto px-4 mb-16 flex justify-center flex-col">
+              <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
+                  {
+                      allPosts.map(post => {
+                          return <Link as={`/playlist/${post.slug}`} href="/playlist/[slug]" key={post.slug}>
+                              {post.frontmatter.name}
+                          </Link>
+                      })
+                  }
+              </div>
+          </div>
       </main>
     </div>
   );
