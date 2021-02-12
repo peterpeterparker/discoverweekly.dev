96fe8ff4b85b0a1700fbc56dbf99987c82e6b613
peterpeterparker <david.dalbusco@outlook.com>
2021-02-12 17:51:11 +0100

    feat: share icon

--- src/components/social/Social.js
+++ src/components/social/Social.js
@@ -3,14 +3,14 @@ import config from "../../config.json";
 import {Twitter} from "../links/Twitter";
 import {GitHub} from "../links/GitHub";
 
-export const Social = ({margin, share}) => {
+export const Social = ({margin}) => {
     return <div className={`flex ${margin}`}>
         <GitHub url={config.github} label="GitHub" small={true}></GitHub>
 
         <Twitter url="https://twitter.com/daviddalbusco" label={`Twitter @daviddalbusco`} small={true}></Twitter>
 
         <a href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fdiscoverweekly.dev&text=Check+out+DiscoverWeekly.dev+by+%40daviddalbusco%20%F0%9F%A4%9F" rel="noopener norefferer" aria-label="Share on Twitter" className="m-2 text-white hover:text-purple-600">
-            <svg className="h-6 w-6 fill-current" viewBox="0 0 512 512"><path d="M496,109.5a201.8,201.8,0,0,1-56.55,15.3,97.51,97.51,0,0,0,43.33-53.6,197.74,197.74,0,0,1-62.56,23.5A99.14,99.14,0,0,0,348.31,64c-54.42,0-98.46,43.4-98.46,96.9a93.21,93.21,0,0,0,2.54,22.1,280.7,280.7,0,0,1-203-101.3A95.69,95.69,0,0,0,36,130.4C36,164,53.53,193.7,80,211.1A97.5,97.5,0,0,1,35.22,199v1.2c0,47,34,86.1,79,95a100.76,100.76,0,0,1-25.94,3.4,94.38,94.38,0,0,1-18.51-1.8c12.51,38.5,48.92,66.5,92.05,67.3A199.59,199.59,0,0,1,39.5,405.6,203,203,0,0,1,16,404.2,278.68,278.68,0,0,0,166.74,448c181.36,0,280.44-147.7,280.44-275.8,0-4.2-.11-8.4-.31-12.5A198.48,198.48,0,0,0,496,109.5Z"/></svg>
+            <svg className="h-6 w-6 fill-current" viewBox='0 0 512 512'><path d='M336 192h40a40 40 0 0140 40v192a40 40 0 01-40 40H136a40 40 0 01-40-40V232a40 40 0 0140-40h40M336 128l-80-80-80 80M256 321V48' fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='32'/></svg>
         </a>
     </div>
 }
