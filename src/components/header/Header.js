09befa6316101761061e9420b722f04282dd4fdf
peterpeterparker <david.dalbusco@outlook.com>
2021-02-14 18:56:55 +0100

    feat: logo maskable favicon

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,8 +9,8 @@ const Header = () => {
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
       <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#004e64" />
-      <meta name="msapplication-TileColor" content="#af2bbf" />
-      <meta name="theme-color" content="#af2bbf" />
+      <meta name="msapplication-TileColor" content="#161b22" />
+      <meta name="theme-color" content="#7C3AED" />
 
       <link rel="manifest" href="/site.webmanifest" />
     </Head>
