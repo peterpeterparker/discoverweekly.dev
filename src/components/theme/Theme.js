efb8fb9c8a97b2a0704fe829e463c350d128a3e1
peterpeterparker <david.dalbusco@outlook.com>
2021-02-19 15:44:34 +0100

    feat: filled icons

--- src/components/theme/Theme.js
+++ src/components/theme/Theme.js
@@ -30,52 +30,46 @@ const Theme = () => {
         } absolute block w-4 h-4 mt-1 ml-1 rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transition-transform duration-300 ease-in-out`}>
         <input type="checkbox" className="absolute opacity-0 w-0 h-0" />
 
-        <svg
-          className={`h-3 w-3 text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-opacity duration-300 ease-in-out ${
-            theme === 'dark' ? 'opacity-100' : 'opacity-0'
-          }`}
-          aria-hidden={true}
-          alt=""
-          viewBox="0 0 512 512">
-          <path
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeMiterlimit="10"
-            strokeWidth="32"
-            d="M256 48v48M256 416v48M403.08 108.92l-33.94 33.94M142.86 369.14l-33.94 33.94M464 256h-48M96 256H48M403.08 403.08l-33.94-33.94M142.86 142.86l-33.94-33.94"
-          />
-          <circle
-            cx="256"
-            cy="256"
-            r="80"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeMiterlimit="10"
-            stroke-width="32"
-          />
-        </svg>
+        {renderSun()}
 
-        <svg
-          className={`h-3 w-3 text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-opacity duration-300 ease-in-out ${
-            theme === 'light' ? 'opacity-100' : 'opacity-0'
-          }`}
-          aria-hidden={true}
-          alt=""
-          viewBox="0 0 512 512">
-          <path
-            d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="32"
-          />
-        </svg>
+        {renderMoon()}
       </span>
     </button>
   );
+
+  function renderSun() {
+    return (
+      <svg
+        className={`h-3 w-3 text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-opacity duration-300 ease-in-out ${
+          theme === 'dark' ? 'opacity-100' : 'opacity-0'
+        }`}
+        aria-hidden={true}
+        alt=""
+        viewBox="0 0 512 512">
+        <path
+          fill="currentColor"
+          d="M256 118a22 22 0 01-22-22V48a22 22 0 0144 0v48a22 22 0 01-22 22zM256 486a22 22 0 01-22-22v-48a22 22 0 0144 0v48a22 22 0 01-22 22zM369.14 164.86a22 22 0 01-15.56-37.55l33.94-33.94a22 22 0 0131.11 31.11l-33.94 33.94a21.93 21.93 0 01-15.55 6.44zM108.92 425.08a22 22 0 01-15.55-37.56l33.94-33.94a22 22 0 1131.11 31.11l-33.94 33.94a21.94 21.94 0 01-15.56 6.45zM464 278h-48a22 22 0 010-44h48a22 22 0 010 44zM96 278H48a22 22 0 010-44h48a22 22 0 010 44zM403.08 425.08a21.94 21.94 0 01-15.56-6.45l-33.94-33.94a22 22 0 0131.11-31.11l33.94 33.94a22 22 0 01-15.55 37.56zM142.86 164.86a21.89 21.89 0 01-15.55-6.44l-33.94-33.94a22 22 0 0131.11-31.11l33.94 33.94a22 22 0 01-15.56 37.55zM256 358a102 102 0 11102-102 102.12 102.12 0 01-102 102z"
+        />
+      </svg>
+    );
+  }
+
+  function renderMoon() {
+    return (
+      <svg
+        className={`h-3 w-3 text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-opacity duration-300 ease-in-out ${
+          theme === 'light' ? 'opacity-100' : 'opacity-0'
+        }`}
+        aria-hidden={true}
+        alt=""
+        viewBox="0 0 512 512">
+        <path
+          fill="currentColor"
+          d="M264 480A232 232 0 0132 248c0-94 54-178.28 137.61-214.67a16 16 0 0121.06 21.06C181.07 76.43 176 104.66 176 136c0 110.28 89.72 200 200 200 31.34 0 59.57-5.07 81.61-14.67a16 16 0 0121.06 21.06C442.28 426 358 480 264 480z"
+        />
+      </svg>
+    );
+  }
 };
 
 export default Theme;
