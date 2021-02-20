698cc4e526f2006f5c89e1d9a2d423a0711c435e
peterpeterparker <david.dalbusco@outlook.com>
2021-02-20 10:08:55 +0100

    feat: easter egg

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,3 +5,15 @@ function MyApp({Component, pageProps}) {
 }
 
 export default MyApp;
+
+// Easter egg
+console.log(`______ _                             _    _           _    _            _            
+|  _  (_)                           | |  | |         | |  | |          | |           
+| | | |_ ___  ___ _____   _____ _ __| |  | | ___  ___| | _| |_   _   __| | _____   __
+| | | | / __|/ __/ _ \\ \\ / / _ \\ '__| |/\\| |/ _ \\/ _ \\ |/ / | | | | / _\` |/ _ \\ \\ / /
+| |/ /| \\__ \\ (_| (_) \\ V /  __/ |  \\  /\\  /  __/  __/   <| | |_| || (_| |  __/\\ V / 
+|___/ |_|___/\\___\\___/ \\_/ \\___|_|   \\/  \\/ \\___|\\___|_|\\_\\_|\\__, (_)__,_|\\___| \\_/  
+                                                              __/ |                  
+                                                             |___/                   `);
+console.log('Contribute now, share your best music tips!');
+console.log('https://github.com/peterpeterparker/discoverweekly.dev#contributing');
